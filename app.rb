5eda565db6b678fe3a6f84a2892293ee1ecfb53e
Adam Duff <j.adam.duff@gmail.com>
2018-09-06 04:30:48 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -12,6 +12,14 @@ module FormsLab
     end
 
     post '/show' do
+      @pirate = Pirate.new(params[:pirate])
+
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+
+      @ships = Ship.all
+
       erb :'pirates/show'
     end
 
